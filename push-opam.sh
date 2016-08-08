488551ad8a686f3944788f9ae712c9f279cd5380
Fabrice Le Fessant <fabrice.le_fessant@ocamlpro.com>
2016-08-08 17:56:33 +0200

    Fix OPAM uninstall

--- push-opam.sh
+++ push-opam.sh
@@ -16,13 +16,18 @@ PACKAGE=${PACKAGE_NAME}
 (cd ${OPAM_REPO} && git checkout master && git pull ${OPAM_REPO_OFFICIAL_REMOTE} master)
 
 
+if [ ! -e ${OPAM_REPO}/packages ]; then
+    echo "Error: directory ${OPAM_REPO}/packages does not exist";
+    exit 2
+fi
+
 if [ -e ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION} ]; then
     echo "Error: directory for ${VERSION} already exists"
     echo ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}
     exit 2
 fi
 
-CMD="mkdir ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}"
+CMD="mkdir -p ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}"
 echo $CMD
 $CMD
 
@@ -34,11 +39,13 @@ URL=${DOWNLOAD_URL_PREFIX}${VERSION}.tar.gz
 TMPFILE=/tmp/push-ocaml.tmp
 rm -f ${TMPFILE}
 
-CMD="wget -o ${TMPFILE} ${URL}"
+CMD="wget -O ${TMPFILE} ${URL}"
 echo $CMD
 $CMD
 
-MD5SUM=$(md5sum ${TMPFILE} | cut -b 1-32) 
+echo Computing checksum on ${TMPFILE}
+md5sum ${TMPFILE}
+MD5SUM=$(md5sum ${TMPFILE} | cut -b 1-32)
 echo 'archive: "'${URL}'"' > ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}/url
 echo 'checksum: "'${MD5SUM}'"' >> ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}/url
 
@@ -49,4 +56,4 @@ cat ${OPAM_REPO}/packages/${PACKAGE}/${PACKAGE}.${VERSION}/url
         git checkout -b ${PACKAGE}.${VERSION} &&
         git add packages/${PACKAGE}/${PACKAGE}.${VERSION} &&
         git commit -m "Add ${PACKAGE}.${VERSION}" packages/${PACKAGE}/${PACKAGE}.${VERSION} &&
-        git push ${OPAM_REPO_FORK_REMOTE} ${PACKAGE}.${VERSION})
+        git push ${OPAM_REPO_FORK_REMOTE} ${PACKAGE}.${VERSION}) || echo "Error: cleanup with git branch -D ${PACKAGE}.${VERSION}"
